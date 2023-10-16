fa44043c9f9db22149b649adcf51b6e9085afc2e
Ilya Kotof <zdobnovi@yandex.ru>
2023-10-16 16:27:42 +0300

    add solution for 3-module 1,3,4-task

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -4,10 +4,19 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+
+    this.sumChunkSize = 0;
+    this.limit = options.limit;
   }
 
   _transform(chunk, encoding, callback) {
+    this.sumChunkSize += Buffer.byteLength(chunk);
 
+    if (this.sumChunkSize > this.limit) {
+      callback(new LimitExceededError());
+    } else {
+      callback(null, chunk);
+    };
   }
 }
 
--- 03-streams/04-file-server-post/server.js
+++ 03-streams/04-file-server-post/server.js
@@ -1,6 +1,9 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+const LimitSizeStream = require('./LimitSizeStream');
+
 
 const server = new http.Server();
 
@@ -10,8 +13,52 @@ server.on('request', (req, res) => {
 
   const filepath = path.join(__dirname, 'files', pathname);
 
+
+  if (pathname.split('/').length > 1) {
+    res.statusCode = 400;
+    res.end('Invalid directory');
+    res.on('error', (error) => {
+      console.log('Ошибка пути');
+    });
+  }
+
+  fs.stat(filepath, (err, stat) => {
+    if (!err) {
+      res.statusCode = 409;
+      res.end('The file already exists');
+      req.push(null);
+    }
+  });
+
+  const limitedStream = new LimitSizeStream({limit: 10, encoding: 'utf-8'});
+
   switch (req.method) {
     case 'POST':
+      const stream = fs.createWriteStream(filepath);
+
+      stream.on('open', () => {
+        req
+            .pipe(limitedStream)
+            .on('error', (error) => {
+              if (error.code === 'LIMIT_EXCEEDED') {
+                res.statusCode = 413;
+                res.end('LIMIT_EXCEEDED');
+              } else {
+                res.statusCode = 500;
+                res.end('Server error');
+              }
+            })
+            .pipe(stream);
+
+            res.end('File created');
+      });
+
+
+      req.on('aborted', () => {
+        stream.destroy();
+        fs.rm( pathname );
+      });
+
 
       break;
 
