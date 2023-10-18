b6280721c8ce4915beaf077e1e96591f433385cf
Ilya Kotof <zdobnovi@yandex.ru>
2023-10-18 19:21:15 +0300

    add solution for 3-module 1,3,4,5-task

--- 03-streams/05-file-server-delete/server.js
+++ 03-streams/05-file-server-delete/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -10,8 +11,25 @@ server.on('request', (req, res) => {
 
   const filepath = path.join(__dirname, 'files', pathname);
 
+  if (pathname.includes('/') || pathname.includes('..')) {
+    res.statusCode = 400;
+    res.end('Nested paths are not allowed');
+    return;
+  }
+
   switch (req.method) {
     case 'DELETE':
+      fs.stat(filepath, (err, stat) => {
+        if (!err) {
+          fs.rm(filepath, () => {
+            res.statusCode = 200;
+            res.end('File deleted');
+          })
+        } else {
+          res.statusCode = 404;
+          res.end('File not found');
+        }
+      });
 
       break;
 
