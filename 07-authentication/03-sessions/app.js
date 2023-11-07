a30ce67a9ff5e39c2a5feab4204b55475606cea5
Ilya Kotof <zdobnovi@yandex.ru>
2023-11-07 11:49:00 +0300

    add solution for 6-module 1,2-task & 7-module 1,2,3-task(3)

--- 07-authentication/03-sessions/app.js
+++ 07-authentication/03-sessions/app.js
@@ -32,6 +32,7 @@ app.use(async (ctx, next) => {
 app.use((ctx, next) => {
   ctx.login = async function(user) {
     const token = uuid();
+    await Session.create({token, user, lastVisit: new Date()});
 
     return token;
   };
@@ -44,6 +45,19 @@ const router = new Router({prefix: '/api'});
 router.use(async (ctx, next) => {
   const header = ctx.request.get('Authorization');
   if (!header) return next();
+
+  const token = header.split(' ')[1];
+  if (!token) return next();
+
+  const session = await Session.findOne({token}).populate('user');
+  if (!session) {
+    ctx.throw(401, 'Неверный аутентификационный токен');
+  }
+  session.lastVisit = new Date();
+  await session.save();
+
+  ctx.user = session.user;
+  return next();
 
   return next();
 });
--- 07-authentication/03-sessions/libs/mustBeAuthenticated.js
+++ 07-authentication/03-sessions/libs/mustBeAuthenticated.js
@@ -1,17 +1,7 @@
-const Session = require('../models/Session');
-
 module.exports = async function mustBeAuthenticated(ctx, next) {
-  const header = ctx.request.get('Authorization');
-
-  if (!header) {
-    ctx.status = 401;
-    ctx.body = {error: 'Пользователь не залогинен'};
-    return;
+  if (!ctx.user) {
+    ctx.throw(401, 'Пользователь не залогинен');
   }
 
-  const token = header.split(' ')[1];
-
-  const session = await Session.findOne({token}).populate('user');
-  ctx.user = session.user;
   return next();
 };
